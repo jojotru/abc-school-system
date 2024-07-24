733e27cdd667149c78a70a6063202038e4e01508
RootBeer8 <109646119+jojotru@users.noreply.github.com>
2024-07-23 23:33:49 -0400

    Update string

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-
+import { BrowserRouter, Routes, Route } from 'react-router'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
